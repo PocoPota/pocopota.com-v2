89ab2b6f7b65b8b37b5d5afe3e1dc7d0d662ac78
PocoPota <pocopota.program@gmail.com>
2025-01-04 13:31:15 +0900

    header確保 (要shadow)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <header>
+  <div class="header-inner">
+    <div class="header-logo">PocoPota.dev</div>
+  </div>
 </header>
 
 <main>
@@ -11,5 +14,22 @@
   // reset css
   :global(body){
     margin: 0;
+  }
+
+  // header
+  header{
+    width: 100%;
+    height: 60px;
+    border-bottom: solid 1px black;
+    .header-inner{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 950px;
+      height: 100%;
+      margin: 0 auto;
+      color: white;
+      font-size: 16px;
+    }
   }
 </style>
