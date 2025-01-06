40d6dd9013f209d02b1756b8c837c6c65c4edfc2
PocoPota <pocopota.program@gmail.com>
2025-01-06 10:41:26 +0900

    header固定

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,30 @@
 <script>
   const year = new Date().getFullYear();
+
+  // headerのぼかし度計算
+  let displayRatio = 0;
+  $: blurRatio = `${displayRatio * 15}px`;
+  $: headerShadow = false;
+
+  if(typeof window !== 'undefined'){
+      const bgBlur = document.querySelector('#bg-blur');
+    window.addEventListener('scroll', () =>{
+      let scrollY = window.scrollY;
+      const bodyHeight = window.outerHeight;
+      if(scrollY / bodyHeight === 0){
+        headerShadow = false;
+      }else if(scrollY / bodyHeight > 1){
+        displayRatio = 1;
+        headerShadow = true;
+      }else{
+        displayRatio = scrollY / bodyHeight;
+        headerShadow = true;
+      }
+    });
+  }
 </script>
 
-<header>
+<header class="{headerShadow ? 'header-scroll' : ''}" style="--blur-ratio: blur({blurRatio});">
   <div class="header-inner">
     <div class="header-logo">PocoPota.com</div>
   </div>
@@ -36,6 +58,13 @@
   header{
     width: 100%;
     height: 60px;
+    position: fixed;
+    top: 0;
+    backdrop-filter: var(--blur-ratio);
+    -webkit-backdrop-filter: var(--blur-ratio);
+    &.header-scroll{
+      box-shadow: 0 4px 12px 2px rgba(255, 255, 255, 0.144);
+    }
     .header-inner{
       display: flex;
       justify-content: center;
