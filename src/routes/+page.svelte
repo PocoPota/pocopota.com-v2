fd8f8110173c01e650b33c5d88bdb0eb58c7b5d8
PocoPota <pocopota.program@gmail.com>
2025-01-06 10:41:49 +0900

    topファイル分け、skills削除

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Top from "./top.svelte";
   import Works from "./works.svelte";
   import Articles from "./articles.svelte";
   import CertAw from "./cert-aw.svelte";
@@ -6,54 +7,7 @@
   export let data;
 </script>
 
-<section class="top">
-  <div class="icon">
-    <img src="/pocopota.png" alt="PocoPota">
-  </div>
-  <div class="about">
-    <div class="name">PocoPota<span>&lbrace;@PocoPota&#125;</span></div>
-    <div class="title-content belong">
-      <div>所属</div>
-      <div>
-        <ul>
-          <li>筑波大学情報学群情報科学類 (25B)</li>
-        </ul>
-      </div>
-    </div>
-    <div class="title-content">
-      <div>技術</div>
-      <div>Web周辺 (特にフロントエンド)</div>
-    </div>
-    <div class="title-content">
-      <div>生息地</div>
-      <div>つくば / 山口</div>
-    </div>
-  </div>
-  <div class="top-links">
-    <ul>
-      <li>
-        <div>Twitter (X)</div>
-        <div>@PocoPota_io</div>
-        <div></div>
-      </li>
-      <li>
-        <div>GitHub</div>
-        <div>PocoPota</div>
-        <div></div>
-      </li>
-      <li>
-        <div>Zenn</div>
-        <div>@pocopota</div>
-        <div></div>
-      </li>
-      <li>
-        <div>Discord</div>
-        <div>pocopota</div>
-        <div></div>
-      </li>
-    </ul>
-  </div>
-</section>
+<Top/>
 
 <section class="works">
   <div class="sec-title">Works</div>
@@ -63,10 +17,6 @@
 <section class="articles">
   <div class="sec-title">Articles</div>
   <Articles/>
-</section>
-
-<section class="skills">
-  <div class="sec-title">Skills</div>
 </section>
 
 <section class="certificates-awards">
@@ -115,53 +65,6 @@
       }
       &::after{
         content: ' }';
-      }
-    }
-  }
-
-  // top
-  .top{
-    display: flex;
-    justify-content: center;
-    margin-top: 130px;
-    gap: 50px;
-    .icon{
-      img{
-        width: 170px;
-        border-radius: 50%;
-      }
-    }
-    .about{
-      display: flex;
-      flex-direction: column;
-      gap: 7px;
-      .name{
-        display: flex;
-        font-size: 28px;
-        font-weight: 800;
-        margin-bottom: 8px;
-        span{
-          display: flex;
-          align-items: center;
-          margin-left: 10px;
-          font-size: 20px;
-          font-family: "IBM Plex Mono", serif;
-        }
-      }
-      .title-content{
-        div{
-          &:nth-child(1){
-            font-size: 13px;
-            color: #cfcfcf;
-          }
-        }
-        &.belong{
-          ul{
-            list-style: none;
-            margin: 0;
-            padding: 0;
-          }
-        }
       }
     }
   }
---
+++ src/routes/top.svelte
@@ -0,0 +1,98 @@
+<section class="top">
+  <div class="icon">
+    <img src="/pocopota.png" alt="PocoPota">
+  </div>
+  <div class="about">
+    <div class="name">PocoPota<span>&lbrace;@PocoPota&#125;</span></div>
+    <div class="title-content belong">
+      <div>所属</div>
+      <div>
+        <ul>
+          <li>筑波大学情報学群情報科学類 (25B)</li>
+        </ul>
+      </div>
+    </div>
+    <div class="title-content">
+      <div>技術</div>
+      <div>Web周辺 (特にフロントエンド)</div>
+    </div>
+    <div class="title-content">
+      <div>生息地</div>
+      <div>つくば / 山口</div>
+    </div>
+  </div>
+  <div class="top-links">
+    <ul>
+      <li>
+        <div>Twitter (X)</div>
+        <div>@PocoPota_io</div>
+        <div></div>
+      </li>
+      <li>
+        <div>GitHub</div>
+        <div>PocoPota</div>
+        <div></div>
+      </li>
+      <li>
+        <div>Zenn</div>
+        <div>@pocopota</div>
+        <div></div>
+      </li>
+      <li>
+        <div>Discord</div>
+        <div>pocopota</div>
+        <div></div>
+      </li>
+    </ul>
+  </div>
+</section>
+
+<style lang="scss">
+    // top
+    .top{
+    display: flex;
+    justify-content: center;
+    margin-top: 250px;
+    gap: 50px;
+    color: white;
+    .icon{
+      img{
+        width: 170px;
+        border-radius: 50%;
+      }
+    }
+    .about{
+      display: flex;
+      flex-direction: column;
+      gap: 7px;
+      .name{
+        display: flex;
+        font-size: 28px;
+        font-weight: 800;
+        margin-bottom: 8px;
+        span{
+          display: flex;
+          align-items: center;
+          margin-left: 10px;
+          font-size: 20px;
+          font-family: "IBM Plex Mono", serif;
+        }
+      }
+      .title-content{
+        div{
+          &:nth-child(1){
+            font-size: 13px;
+            color: #cfcfcf;
+          }
+        }
+        &.belong{
+          ul{
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        }
+      }
+    }
+  }
+</style>
