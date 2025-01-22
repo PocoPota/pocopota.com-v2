aec27f12d21ebf70650ae3d309b03bae92be2036
PocoPota <pocopota.program@gmail.com>
2025-01-22 22:37:23 +0900

    記事データの表示

--- src/routes/articles.svelte
+++ src/routes/articles.svelte
@@ -1,18 +1,48 @@
 <script>
+  import { urlencoded } from "express";
+
   export let rss_data;
+
+  // 記事データの取得
+  let zennArticles = rss_data.zenn.rss.channel.item;
+  let hatenaArticles = rss_data.hatena.rss.channel.item;
+  let blogArticles = rss_data.blog.rss.channel.item;
+
+  // 記事を統合
+  let articles = [
+    ...zennArticles.slice(0, 6),
+    ...hatenaArticles.slice(0, 6),
+    ...blogArticles.slice(0, 6)
+  ];
+
+  // 全ての記事を統合・時系列に並べる
+  articles.sort((a, b) => {
+    return new Date(b.pubDate) - new Date(a.pubDate);
+  });
+
+  // 記事を6件に絞る
+  articles = articles.slice(0, 6);
+
+  // 時間形式の変換関数
+  function formatDate(dateStr) {
+    return new Date(dateStr).toISOString().slice(0, 10).replace(/-/g, '/');
+  }
 </script>
 
 <ul>
-  <li>
-    <img src="/ex-thumb.png" alt="サムネイル">
-    <div>
-      <div>記事タイトル</div>
+  {#each articles as item}
+    <li>
+      <!-- <img src={item.enclosure.urlencoded.url} alt="サムネイル"> -->
+      <img src="/ex-thumb.png" alt="サムネイル">
       <div>
-        <span>zenn.dev</span>
-        <span>2025/01/04</span>
+        <div>{item.title}</div>
+        <div>
+          <span>{item.source}</span>
+          <span>{formatDate(item.pubDate)}</span>
+        </div>
       </div>
-    </div>
-  </li>
+    </li>
+  {/each}
 </ul>
 
 <style lang="scss">
