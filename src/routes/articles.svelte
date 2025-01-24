3ca6d7fe0186da6ce364efc1a6c67d3ef2211461
PocoPota <pocopota.program@gmail.com>
2025-01-24 15:00:03 +0900

    記事部分デザイン

--- src/routes/articles.svelte
+++ src/routes/articles.svelte
@@ -30,17 +30,23 @@
 <ul>
   {#each articles as item}
     <li>
-      <img src={item.enclosure['$'].url} alt="サムネイル">
-      <div>
-        <div>{item.title}</div>
-        <div>
-          <span>{item.source}</span>
-          <span>{formatDate(item.pubDate)}</span>
+      <a href="{item.link}" target="_blank" rel="noopener noreferrer">
+        <div class="thumbnail">
+          <img src={item.enclosure['$'].url} alt="サムネイル">
         </div>
-      </div>
+        <div class="content">
+          <div>{item.title}</div>
+          <div>
+            <span>{item.source}</span>
+            <span>{formatDate(item.pubDate)}</span>
+          </div>
+        </div>
+      </a>
     </li>
   {/each}
 </ul>
+
+<div class="more-read"><a href="/articles">もっと見る</a></div>
 
 <style lang="scss">
   ul{
@@ -52,8 +58,45 @@
     padding: 0;
     li{
       width: 300px;
-      img{
+      &:hover{
+        filter: brightness(1.3);
+      }
+      a{
+        color: white;
+        text-decoration: none;
+      }
+      .thumbnail{
         width: 100%;
+        height: 157.5px;
+        overflow: hidden;
+        img{
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+      .content{
+        margin-top: 7px;
+        div{
+          margin-bottom: 2px;
+          font-size: 16px;
+          line-height: 1.1em;
+        }
+        span{
+          font-size: 13px;
+        }
+      }
+    }
+  }
+  .more-read{
+    margin-top: 40px;
+    text-align: center;
+    font-size: 14px;
+    a{
+      color: white;
+      text-decoration: none;
+      &:hover{
+        text-decoration: underline;
       }
     }
   }
