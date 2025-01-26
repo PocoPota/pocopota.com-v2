f048349d7223bd3238d305ce608c7e3d37febb4e
PocoPota <pocopota.program@gmail.com>
2025-01-26 20:47:58 +0900

    一旦記事ページの追加

---
+++ src/routes/articles/+page.server.js
@@ -0,0 +1,38 @@
+import { VITE_BLOG_RSS_URL, VITE_HATENA_RSS_URL, VITE_ZENN_RSS_URL } from "$env/static/private";
+import xml2js from 'xml2js';
+
+export async function load({ fetch }) {
+  // RSS
+  try{
+    // Zenn
+    const zenn_request_url = VITE_ZENN_RSS_URL;
+    const zenn_response = await fetch(zenn_request_url);
+    // Hatena
+    const hatena_request_url = VITE_HATENA_RSS_URL;
+    const hatena_response = await fetch(hatena_request_url);
+    // Blog
+    const blog_request_url = VITE_BLOG_RSS_URL;
+    const blog_response = await fetch(blog_request_url);
+    // Check response
+    if(!zenn_response.ok || !hatena_response.ok || !blog_response.ok){
+      throw new Error(`Failed to fetch: ${zenn_response.status} ${hatena_response.status} ${blog_response.status}`);
+    }
+    let rss_data = {
+      zenn: await zenn_response.text(),
+      hatena: await hatena_response.text(),
+      blog: await blog_response.text()
+    };
+
+    const parser = new xml2js.Parser({ explicitArray: false, trim: true });
+    rss_data.zenn = await parser.parseStringPromise(rss_data.zenn);
+    rss_data.hatena = await parser.parseStringPromise(rss_data.hatena);
+    rss_data.blog = await parser.parseStringPromise(rss_data.blog);
+
+    return {
+      rss_data
+    };
+  }
+  catch(e){
+    return {'error': true, 'message': e.message};
+  }
+}
---
+++ src/routes/articles/+page.svelte
@@ -0,0 +1,90 @@
+<script>
+  export let data;
+
+  // 記事データの取得
+  let zennArticles = data.rss_data.zenn.rss.channel.item;
+  let hatenaArticles = data.rss_data.hatena.rss.channel.item;
+  let blogArticles = data.rss_data.blog.rss.channel.item;
+
+  // 記事を統合
+  let articles = [
+    ...zennArticles,
+    ...hatenaArticles,
+    ...blogArticles
+  ];
+
+  // 全ての記事を時系列に並べる
+  articles.sort((a, b) => {
+    return new Date(b.pubDate) - new Date(a.pubDate);
+  });
+
+  // 時間形式の変換関数
+  function formatDate(dateStr) {
+    return new Date(dateStr).toISOString().slice(0, 10).replace(/-/g, '/');
+  }
+</script>
+
+<ul>
+  {#each articles as item}
+    <li>
+      <a href="{item.link}" target="_blank" rel="noopener noreferrer">
+        <div class="thumbnail">
+          {#if item.enclosure}
+            <img src={item.enclosure['$'].url} alt="サムネイル">
+          {:else}
+            <div>無理だぜ！！！！！！！！！！！！</div>
+          {/if}
+        </div>
+        <div class="content">
+          <div>{item.title}</div>
+          <div>
+            <span>{item.source}</span>
+            <span>{formatDate(item.pubDate)}</span>
+          </div>
+        </div>
+      </a>
+    </li>
+  {/each}
+</ul>
+
+<style lang="scss">
+  ul{
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 25px;
+    list-style: none;
+    padding: 0;
+    li{
+      width: 300px;
+      &:hover{
+        filter: brightness(1.3);
+      }
+      a{
+        color: white;
+        text-decoration: none;
+      }
+      .thumbnail{
+        width: 100%;
+        height: 157.5px;
+        overflow: hidden;
+        img{
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+      .content{
+        margin-top: 7px;
+        div{
+          margin-bottom: 2px;
+          font-size: 16px;
+          line-height: 1.1em;
+        }
+        span{
+          font-size: 13px;
+        }
+      }
+    }
+  }
+</style>
