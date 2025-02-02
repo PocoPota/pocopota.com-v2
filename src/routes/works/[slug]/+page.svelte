3945b24487fb25e226dc1b3f507902857c526e46
PocoPota <pocopota.program@gmail.com>
2025-02-02 23:33:45 +0900

    作品ページ：データの簡易表示

--- src/routes/works/[slug]/+page.svelte
+++ src/routes/works/[slug]/+page.svelte
@@ -1,6 +1,25 @@
 <script>
   export let data;
+  const item = data.item_data;
 </script>
 
-<h1>{data.slug}</h1>
-作品詳細
+<h1>{item.fields.name}</h1>
+<img src={item.fields.thumbnail.fields.file.url} alt={item.fields.name}>
+
+<ul>
+  {#each item.fields.devs as tool}
+    <li>{tool}</li>
+  {/each}
+</ul>
+
+<div>作品URL：{item.fields.url}</div>
+<div>GitHub URL：{item.fields.repository}</div>
+
+<style lang="scss">
+  *{
+    color: white;
+  }
+  img{
+    width: 300px;
+  }
+</style>
