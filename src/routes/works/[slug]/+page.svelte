e36c4d1be6554734edd4a0847cdd715cd1950e7f
PocoPota <pocopota.program@gmail.com>
2025-02-06 14:47:53 +0900

    スタイルの調整

--- src/routes/works/[slug]/+page.svelte
+++ src/routes/works/[slug]/+page.svelte
@@ -11,8 +11,9 @@
       <img src={item.fields.thumbnail.fields.file.url} alt={item.fields.name}>
     </div>
     <div class="details">
-      <div class="date">{item.fields.date}</div>
+      <div class="date">公開時期：{item.fields.date}</div>
       <div class="tools">
+        <div>使用技術：</div>
         <ul>
           {#each item.fields.devs as tool}
             <li>{tool}</li>
@@ -27,7 +28,7 @@
   </div>
 
   <div class="row-right">
-    <ul>
+    <ul class="comments">
       {#each item.fields.comments as comment}
         <li>{comment}</li>
       {/each}
@@ -38,6 +39,45 @@
 <style lang="scss">
   *{
     color: white;
+  }
+
+  h1{
+    margin-top: 150px;
+    font-size: 32px;
+    font-weight: 600;
+    text-align: center;
+    &::before{
+      font-family: "IBM Plex Mono", serif;
+      content: '{ ';
+    }
+    &::after{
+      font-family: "IBM Plex Mono", serif;
+      content: ' }';
+    }
+  }
+
+  section{
+    width: 950px;
+    margin: 50px auto;
+    display: flex;
+    gap: 30px;
+    .row-left{
+      width: 500px;
+      .thumbnail{
+        img{
+          width: 100%;
+        }
+      }
+    }
+    .row-right{
+      .comments{
+        padding: 0;
+        margin: 0;
+        li{
+          list-style: none;
+        }
+      }
+    }
   }
   img{
     width: 300px;
