fc8a8c0115ea8e6916143a2b2724f5adcf066def
pocopota <pocopota.program@gmail.com>
2025-07-14 20:40:24 +0900

    初期背景画像を設定

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,4 +1,6 @@
 import { UNSPLASH_ACCESS_KEY, UNSPLASH_API_URL } from "$env/static/private";
+import fs from 'fs/promises';
+import path from "path";
 
 // 背景画像の取得
 export async function load({ fetch }) {
@@ -13,6 +15,15 @@ export async function load({ fetch }) {
     }
     img_data = await img_response.json();
   }catch(e){
+    try{
+      const filePath = path.resolve('static/datas/default-background-image.json');
+      const fileContent = await fs.readFile(filePath, 'utf-8');
+      const data = JSON.parse(fileContent);
+      img_data = data;
+    }
+    catch(e){
+      img_data = {'error': true, 'message': e.message};
+    }
     img_data = {'error': true, 'message': e.message};
   }
 
