453109bc220c9aa203e662bfeadc80dba3ffbae9
pocopota <pocopota.program@gmail.com>
2025-12-10 23:59:49 +0900

    スキルページの追加

---
+++ src/routes/skills/+page.svelte
@@ -0,0 +1,41 @@
+<svelte:head>
+  <title>Skills | PocoPota</title>
+  <meta property="og:title" content="Articles | PocoPota" />
+  <meta property="og:url" content="https://pocopota.io/articles" />
+</svelte:head>
+
+<article>
+  <h1>Skills</h1>
+  
+  <p style="text-align: center;">調整中...</p>
+</article>
+
+<style lang="scss">
+  * {
+    color: white;
+  }
+  h1 {
+    font-size: 32px;
+    font-weight: 600;
+    text-align: center;
+    font-family: "IBM Plex Mono", serif;
+    &::before {
+      content: "{ ";
+    }
+    &::after {
+      content: " }";
+    }
+  }
+
+  section{
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  // スマホレスポンシブ
+  @media screen and (max-width: 500px) {
+    h1 {
+      margin: 30px auto;
+    }
+  }
+</style>
